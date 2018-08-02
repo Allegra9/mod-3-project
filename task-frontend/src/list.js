96da572b91e4fa803c3d74516335b2078f82e5e5
Allegra <aukseallegra@gmail.com>
2018-08-02 10:21:48 +0100

    styling

--- task-frontend/src/list.js
+++ task-frontend/src/list.js
@@ -23,7 +23,7 @@ lastListId()
       <div id="list-${lastId}">
         <input class="check-list" type="checkbox">
         <span id="list-name">${newListName}</span>
-        <span id="list-button-container" style="float:right;">
+        <span id="list-button-container">
           <button class="edit-list fas fa-edit" style="background:transparent; border:none" title="edit"></button>
           <button class="delete-list fas fa-times" style="background:transparent; border:none" title="delete"></button>
         </span>
@@ -115,7 +115,7 @@ function renderLists(data) {
       <div id="list-${listItem.id}">
         <input class="check-list" type="checkbox" ${listItem.checked === "true" && "checked"}>
         <span id="list-name" ${listItem.checked === "true" && 'style="text-decoration:line-through; color:grey;"'}>${listItem.name}</span>
-        <span id="list-button-container" style="float:right;">
+        <span id="list-button-container">
           <button class="edit-list fas fa-edit" style="background:transparent; border:none" title="edit"></button>
           <button class="delete-list fas fa-times" style="background:transparent; border:none" title="delete"></button>
         </span>
